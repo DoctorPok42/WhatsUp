c442ac217509a4b7a6b0a80d54d0993f80da0542
DoctorPok <58393254+DoctorPok42@users.noreply.github.com>
2024-02-09 09:49:44 +0100

    Feat -> initial foundations: front

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    domains: ['img.icons8.com'],
+  },
+};
+
+export default nextConfig;
---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>WhatsUp</title>
+        <meta name="description" content="WhatsUp" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#5ad27d" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="container">
+      </main>
+    </>
+  );
+}
