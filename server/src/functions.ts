39d20e14d252cbf32273c53b7dfabc8c86b22bac
DoctorPok <58393254+DoctorPok42@users.noreply.github.com>
2024-02-11 20:43:07 +0100

    Feat -> token: add authToken with for request

--- server/src/functions.ts
+++ server/src/functions.ts
@@ -1,14 +1,24 @@
 import chalk from "chalk"
-import mongoose from "mongoose";
+import jwt from 'jsonwebtoken'
 
 const themeColors = {
-    text: "#2B2",
-    variable: "#42f5e0",
-    error: "#f5426c"
+  text: "#2B2",
+  variable: "#42f5e0",
+  error: "#f5426c"
 } as any
 
 export const getThemeColor = (color: any) => Number(`0x${themeColors[color].substring(1)}`)
 
 export const color = (color: any, message: any) => {
-    return chalk.hex(themeColors[color])(message)
+  return chalk.hex(themeColors[color])(message)
+}
+
+export const createAuthToken = (id: string) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET as string, {
+      expiresIn: process.env.TOKEN_EXPIRATION
+  });
+}
+
+export const verifyAuthToken = (token: string) => {
+  return jwt.verify(token, process.env.JWT_SECRET as string);
 }
