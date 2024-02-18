6a9948e443fbf13a526e523e64be795db2c2cc88
DoctorPok <58393254+DoctorPok42@users.noreply.github.com>
2024-02-18 17:59:43 +0100

    Feat -> app socket: add universal socket point

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,18 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import Cookies from "universal-cookie";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+import io from 'socket.io-client';
+
+export const socket = io("http://localhost:8000", { transports: ["websocket"] });
+
+const cookies = new Cookies();
+export const token = cookies.get("token");
+
+socket.emit("join", { token });
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
