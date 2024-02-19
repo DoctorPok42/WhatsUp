7da93d7cf1416b7790ac08a09c0e1e822dff3512
DoctorPok <58393254+DoctorPok42@users.noreply.github.com>
2024-02-19 11:00:33 +0100

    Feat -> date and style: add last hour and --dark-blue

--- src/tools/formatDate.ts
+++ src/tools/formatDate.ts
@@ -6,7 +6,9 @@ const formatDate = (date: Date, isLargeDate?: boolean) => {
   const today = new Date();
   const yesterday = new Date(today);
   yesterday.setDate(yesterday.getDate() - 1);
-  if (date.toDateString() === today.toDateString()) {
+  if (date.toDateString() === today.toDateString() && date.getHours() === today.getHours()) {
+    return `${today.getMinutes() - date.getMinutes()} m`;
+  } else if (date.toDateString() === today.toDateString()) {
     return date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' }).toLowerCase();
   } else if (date.toDateString() === yesterday.toDateString()) {
     return 'Yesterday';
