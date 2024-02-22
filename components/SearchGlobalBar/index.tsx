91a04d367925f19d352bfab9237ca6e73d527537
DoctorPok <58393254+DoctorPok42@users.noreply.github.com>
2024-02-22 21:12:45 +0100

    Feat -> messageSearch and messageToLoad

--- components/SearchGlobalBar/index.tsx
+++ components/SearchGlobalBar/index.tsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClose, faSearch } from '@fortawesome/free-solid-svg-icons';
 import emitEvent from '@/tools/webSocketHandler';
-import Image from 'next/image';
+import UserResult from './userReasult';
+import MessageResult from './messageReasult';
 
 import styles from './style.module.scss';
 
@@ -14,6 +15,9 @@ interface SearchGlobalBarProps {
   conversationId?: string
   usersConversation: string[]
   getConversations?: () => void
+  state: "message" | "user"
+  setSearchState: (e: "message" | "user") => void
+  setAllMessages?: (e: any[]) => void
 }
 
 const SearchGlobalBar = ({
@@ -24,6 +28,9 @@ const SearchGlobalBar = ({
   conversationId,
   usersConversation,
   getConversations,
+  state,
+  setSearchState,
+  setAllMessages,
 }: SearchGlobalBarProps) => {
   const [isSearching, setIsSearching] = useState<boolean>(false)
   const [userSearchedAdd, setUserSearchedAdd] = useState<{
@@ -31,20 +38,34 @@ const SearchGlobalBar = ({
     phone: string,
     username: string
   }[]>([])
+  const [messageSearched, setMessageSearched] = useState<any[]>([])
   const [loading, setLoading] = useState<boolean>(false)
 
   if (!isOpen) return null
 
   const onSearch = (search: string) => {
     if (search.trim().length < 1) {
-      setIsSearching(false)
       setUserSearchedAdd([])
+      setMessageSearched([])
+      setIsSearching(false)
       return
     }
 
     setLoading(true)
     setIsSearching(true)
 
+    state === 'user' ? onUserSearch(search) : onMessageSearch(search)
+  }
+
+  const handleClose = () => {
+    setUserSearchedAdd([])
+    setMessageSearched([])
+    setIsSearching(false)
+    setSearchState('user')
+    setIsOpen && setIsOpen(false)
+  }
+
+  const onUserSearch = (search: string) => {
     emitEvent("userSearch", { token, arg: search.trim() }, (data: any) => {
       if (data.status === "success") {
         let newUsers = data.data.map((e: any) => ({
@@ -59,12 +80,6 @@ const SearchGlobalBar = ({
     })
   }
 
-  const handleClose = () => {
-    setUserSearchedAdd([])
-    setIsSearching(false)
-    setIsOpen && setIsOpen(false)
-  }
-
   const handleAddUser = (userId: string) => {
     if (usersConversation.find((e: string) => e === userId)) return
 
@@ -75,6 +90,25 @@ const SearchGlobalBar = ({
         alert(data.message)
       }
     })
+  }
+
+  const onMessageSearch = (message: string) => {
+    if (message.length < 2) return
+    emitEvent('searchMessage', { token, conversationId: conversationId, message: message }, (response) => {
+      if (response.status === 'success') {
+        setMessageSearched(response.data)
+        setLoading(false)
+      }
+    });
+  }
+
+  const handleGoToMessage = (messageId: string) => {
+    emitEvent('getMessage', { token, conversationId, messageId }, (response) => {
+      if (response.status === 'success') {
+        setAllMessages && setAllMessages(response.data)
+      }
+    });
+    handleClose()
   }
 
   return (
@@ -92,7 +126,9 @@ const SearchGlobalBar = ({
           </div>
 
           <div className={styles.input}>
-            <input type="text" placeholder="Search" onChange={(e: any) => onSearch(e.target.value)} autoFocus />
+            <input type="text" placeholder={
+              `Search ${state}...`
+            } onChange={(e: any) => onSearch(e.target.value)} autoFocus />
           </div>
 
           {<div className={styles.iconClose} onClick={handleClose}>
@@ -100,25 +136,12 @@ const SearchGlobalBar = ({
           </div>}
         </div>
 
-        {(userSearchedAdd.length < 1 && isSearching && !loading) && <div className={styles.noUserFound}>
-          <h2>😢 No user found!</h2>
+        {(userSearchedAdd.length < 1 && isSearching && !loading && state === 'user') && <div className={styles.noUserFound}>
+          <h2>😢 No {state} found</h2>
         </div>}
 
-        <div className={styles.userSearched} style={{
-          padding: (isSearching && userSearchedAdd.length) ? '0.6em 0.5em' : '0 0.5em',
-        }}>
-          {userSearchedAdd.map((user, index) => (
-            <div key={index} className={styles.userCard} onClick={() => handleAddUser(user.id)}>
-              <Image src={`https://api.dicebear.com/7.x/avataaars/png?seed=${user.phone}&radius=22&backgroundColor=65c9ff,b6e3f4,c0aede,d1d4f9,ffd5dc,ffdfbf&randomizeIds=true`} alt="userCardIcon" width={50} height={50} />
-              <div className={styles.userInfo}>
-                <h3>{user.username}</h3>
-                <p>{user.phone.replace(/(\d{2})(?=\d)/g, "$1 ")}</p>
-
-                {usersConversation.find((e: string) => e === user.id) && <span>User already in</span>}
-              </div>
-            </div>
-          ))}
-          </div>
+        {state === 'user' && <UserResult isSearching={isSearching} userSearchedAdd={userSearchedAdd} usersConversation={usersConversation} handleAddUser={handleAddUser} />}
+        {state === 'message' && <MessageResult messageSearched={messageSearched} handleGoToMessage={handleGoToMessage} />}
       </div>
     </div>
   );
