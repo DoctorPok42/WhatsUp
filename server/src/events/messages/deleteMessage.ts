8f243305d7206a0abad89366ebcafe68e5dc0fd3
DoctorPok <58393254+DoctorPok42@users.noreply.github.com>
2024-03-09 13:58:16 +0100

    Fix -> deleteMessage: if it's the last one update conversation

--- server/src/events/messages/deleteMessage.ts
+++ server/src/events/messages/deleteMessage.ts
@@ -12,8 +12,16 @@ const deleteMessage = async ({ token, conversationId, messageId }: { token: stri
     const realId = new mongoose.Types.ObjectId(messageId);
 
     // find and delete the message from the conversation
-    const messageToDelete = await mongoose.connection.db.collection(`conversation_${conversationId}`).findOneAndDelete({ _id: realId });
+    const messageToDelete = await mongoose.connection.db.collection(`conversation_${conversationId}`).findOne({ _id: realId });
     if (!messageToDelete) return { status: "error", message: "Message not found." };
+
+    const lastMessage = await mongoose.connection.db.collection(`conversation_${conversationId}`).find().sort({ _id: -1 }).limit(1).toArray();
+    if (lastMessage[0]._id.toString() === messageId) {
+      const secondLastMessage = await mongoose.connection.db.collection(`conversation_${conversationId}`).find().sort({ _id: -1 }).skip(1).limit(1).toArray();
+      await mongoose.connection.db.collection("conversations").updateOne({ _id: new mongoose.Types.ObjectId(conversationId) }, { $set: { lastMessage: secondLastMessage[0].content, lastMessageDate: secondLastMessage[0].date, lastMessageAuthorId: secondLastMessage[0].authorId } });
+    }
+
+    await mongoose.connection.db.collection(`conversation_${conversationId}`).deleteOne({ _id: realId });
 
     return { status: "success", message: "Message deleted." };
 
