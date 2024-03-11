f3535d55ed0c877fa877cb53524a52f2811a72c5
DoctorPok <58393254+DoctorPok42@users.noreply.github.com>
2024-03-11 18:57:55 +0100

    Fix -> Sidebar: icons

--- components/SideBar/index.tsx
+++ components/SideBar/index.tsx
@@ -33,7 +33,7 @@ const SideBar = ({
             <div key={index} className={styles.button} onClick={() => router.push(button.path)} style={{
               color: path === button.path ? '#5ad27d' : '#8393a3',
             }}>
-              <FontAwesomeIcon className={styles.icon} icon={button.icon} width={50} height={50} />
+              <FontAwesomeIcon className={styles.icon} icon={button.icon} width={20} height={20} />
               <a href={button.path}>{button.name}</a>
             </div>
           ))}
