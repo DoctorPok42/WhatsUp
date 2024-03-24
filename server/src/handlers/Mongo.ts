7ff6cd638fad431774b85c6e0566a7dd5fe3c48f
DoctorPok <58393254+DoctorPok42@users.noreply.github.com>
2024-03-24 16:55:07 +0100

    Fix -> delete mongo service

--- server/src/handlers/Mongo.ts
+++ server/src/handlers/Mongo.ts
@@ -2,13 +2,31 @@ import mongoose from "mongoose";
 import { color } from "../functions";
 
 module.exports = () => {
-    const MONGO_URI = process.env.MONGO_URI
+  const MONGO_URI = process.env.MONGO_URI;
 
-    if (!MONGO_URI) return console.log(color("text",`🍃 Mongo URI not found, ${color("error", "skipping.")}`))
+  if (!MONGO_URI)
+    return console.log(
+      color("text", `🍃 Mongo URI not found, ${color("error", "skipping.")}`)
+    );
 
-    mongoose.set("strictQuery", false)
+  mongoose.set("strictQuery", false);
 
-    mongoose.connect(MONGO_URI)
-    .then(() => console.log(color("text",`🍃 MongoDB connection has been ${color("variable", "established.")}`)))
-    .catch(() => console.log(color("text",`🍃 MongoDB connection has been ${color("error", "failed.")}`)))
-}
+  mongoose
+    .connect(MONGO_URI)
+    .then(() =>
+      console.log(
+        color(
+          "text",
+          `🍃 MongoDB connection has been ${color("variable", "established.")}`
+        )
+      )
+    )
+    .catch(() =>
+      console.log(
+        color(
+          "text",
+          `🍃 MongoDB connection has been ${color("error", "failed.")}`
+        )
+      )
+    );
+};
